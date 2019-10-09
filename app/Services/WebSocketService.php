75b7434670ba5dbdd48bf185a216661a38f475b3
Yexk <yexk@yexk.cn>
2019-10-09 08:09:43 +0800

    [add] test tasks

--- app/Services/WebSocketService.php
+++ app/Services/WebSocketService.php
@@ -2,13 +2,18 @@
 
 namespace App\Services;
 
+use App\Tasks\TestTask;
 use Hhxsv5\LaravelS\Swoole\WebSocketHandlerInterface;
 use Swoole\Http\Request;
 use Swoole\WebSocket\Frame;
 use Swoole\WebSocket\Server;
+// 实例化TestTask并通过deliver投递，此操作是异步的，投递后立即返回，由Task进程继续处理TestTask中的handle逻辑
+use Hhxsv5\LaravelS\Swoole\Task\Task;
 
 class WebSocketService implements WebSocketHandlerInterface
 {
+    protected $ws;
+
     // 声明没有参数的构造函数
     public function __construct()
     { }
@@ -19,6 +24,11 @@ class WebSocketService implements WebSocketHandlerInterface
         // 所以Laravel的Request、Auth等信息是可读的，Session是可读写的，但仅限在onOpen事件中。
         // \Log::info('New WebSocket connection', [$request->fd, request()->all(), session()->getId(), session('xxx'), session(['yyy' => time()])]);
         $server->push($request->fd, 'Welcome to LaravelS');
+
+        $task = new TestTask('task data');
+        // $task->delay(3);// 延迟3秒投放任务
+        $ret = Task::deliver($task);
+        var_dump($ret); //判断是否投递成功
         // throw new \Exception('an exception');// 此时抛出的异常上层会忽略，并记录到Swoole日志，需要开发者try/catch捕获处理
     }
 
